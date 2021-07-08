b3d19277d7f2f828537f259b858aaee36f8815a3
Mohit Nagarkoti <47345330+mohitnagarkotibca@users.noreply.github.com>
2021-07-08 15:41:26 +0530

    updated structure

--- Twitter_Sentiment_Analysis/twitter_scrapper.py
+++ Twitter_Sentiment_Analysis/twitter_scrapper.py
@@ -9,12 +9,20 @@ from twitterscraper import query_tweets
 import datetime as dt
 import pandas as pd
 
-begin_date= dt.date(2020,7,2)
-end_date= dt.date(2020,7,3)
+begin_date= dt.date(2021,7,5)
+end_date= dt.date(2021,7,6)
 limit=10
 lang= 'english'
 #user= 'narendra modi'
-tweets= query_tweets("PM Modi", begindate= begin_date, enddate=end_date,limit=limit,lang=lang)
+tweets= query_tweets(query="PM Modi", begindate= begin_date, enddate=end_date,limit=limit,lang=lang)
+
+
+
+
+
+
+
+query_tweets(,)
 df= pd.DataFrame(t.__dict__ for t in tweets)[['screen_name','text']]
 
 
