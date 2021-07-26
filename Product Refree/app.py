7deb3e2ac11da4170ff224058d4c35c6b03592f4
Mohit <mohitnagarkotibca@gmail.com>
2021-07-26 10:48:59 -0700

    Built multi links generator and working properly, BUT, DRIVER CHNAGING RIGID ON ITS PATH

--- Product Refree/app.py
+++ Product Refree/app.py
@@ -30,7 +30,7 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 driver = Edge(executable_path=f'{os.getcwd()}/driver/msedgedriver.exe',options = options)
 
-# give me a url and i will give back DF with all the reviews
+# give me a url and i will give back a list of short reviews and long reviews
 def get_URL(url):
     Enter_URL=url
     driver.get(Enter_URL)
@@ -58,11 +58,57 @@ def get_URL(url):
             except NoSuchElementException:
                 continue   
             clear_output(wait=True)
-    d = zip(SR,LR)
-    mapped = list(d)
-    df = pd.DataFrame(mapped, columns =['Short_review', 'Long_Review'])
-    return df
+    return SR,LR
+    # d = zip(SR,LR)
+    # mapped = list(d)
+    # df = pd.DataFrame(mapped, columns =['Short_review', 'Long_Review'])
+    # return df
     
 
 # url='https://www.amazon.com/HP-15-Athlon-Gold-Bluetooth/dp/B08RCW9PNH/ref=sr_1_1_sspa?dchild=1&keywords=HP%2BPavilion%2B15&qid=1627282296&sr=8-1-spons&smid=AWVDPEZSR45X1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMFM0UFM2S1pQNkRWJmVuY3J5cHRlZElkPUEwMTY2NDMxMzVJOFBBSEIwQVFIVSZlbmNyeXB0ZWRBZElkPUEwODU3MTMyMlRONk9NMjZHQjFOTiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU&th=1'
-df= get_URL(url)
+# df= get_URL(url)
+
+
+#Takes a text, searches for different products,find URLS and Product names, 
+#find reviews of each URL and returns a SINGLE DF
+
+def get_product_URL(text):
+# timeout:exception WebDriverWait(driver,20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR ,'div[data-asin]')))
+    meta_URL= text.replace(' ','+')
+    mega_URL= f'https://www.amazon.in/s?k={meta_URL}&ref=nb_sb_noss_2'
+    print(mega_URL)
+    driver.get(mega_URL)
+    elements = WebDriverWait(driver,20).until(EC.visibility_of_any_elements_located((By.CSS_SELECTOR ,'div[data-asin]')))
+    print(len(elements))
+    links=[]
+    titles=[]
+    prices=[]
+    S_reviews=[]
+    L_reviews=[]
+    for i,item in enumerate(elements):    
+        link=item.find_element_by_tag_name('a').get_attribute('href')
+        SR,LR= get_URL(link)
+        
+        #Yaha pe driver ko malum nahi chall paa raha hai ki, Driver abhi konse link mai hai
+        title=item.find_element_by_tag_name('span').text
+        try:
+            price= item.find_element_by_class_name('a-price').text
+            # print("Elements :",i,title[:10], price)
+            links.append(link)
+            title.append(title)
+            prices.append(price)
+            S_reviews.append(SR)
+            L_reviews.append(LR)
+            if i== len(elements)-1:
+                break
+            
+        except NoSuchElementException:
+            print('error',i,' :element not found')
+            continue
+    df= pd.DataFrame({'Title':titles,'Links':links,'prices':prices,'Short reviews':S_reviews,'Long Reviews':L_reviews})
+    return df
+    
+df= get_product_URL('Best Laptop under 40000')
+
+
+
